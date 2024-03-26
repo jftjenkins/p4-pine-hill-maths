235415e20b68e8d29ba09433c50795b732db36aa
jftjenkins <jft.jenkins@gmail.com>
2024-03-26 08:38:37 +0000

    fix: Uninstalled django-suit and added view for maths_lessons.

--- maths_lessons/views.py
+++ maths_lessons/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-# Create your views here.
 def index(request):
     return HttpResponse("Hello, world!")
+
+def maths_lessons_view(request):
+    # Your view logic here
+    return render(request, 'maths_lessons/lessons.html', context)
--- pine_hills_maths/settings.py
+++ pine_hills_maths/settings.py
@@ -74,7 +74,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'pine_hills_maths.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
