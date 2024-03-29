e20ff37e3e515bd7fc87f60a33644b4f8757f50f
jftjenkins <jft.jenkins@gmail.com>
2024-03-29 13:16:58 +0000

    style: Updated staticfiles with css and fixed issue with heading on student and teacher signin pages.

--- student_auth/models.py
+++ student_auth/models.py
@@ -1,3 +1,25 @@
+from django.contrib.auth.models import User
 from django.db import models
+import random
+import string
 
-# Create your models here.
+class Student(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    username = models.CharField(max_length=4)
+    password = models.CharField(max_length=3)
+
+    @staticmethod
+    def generate_username():
+        return ''.join(random.choices(string.digits, k=4))
+
+    @staticmethod
+    def generate_password():
+        return ''.join(random.choices(string.ascii_lowercase, k=3))
+
+    @classmethod
+    def create_student(cls):
+        user = User.objects.create_user(username=cls.generate_username(),
+                                        password=cls.generate_password())
+        return cls.objects.create(user=user, username=user.username, password=user.password)
+
+
